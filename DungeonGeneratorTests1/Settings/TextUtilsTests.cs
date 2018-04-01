d0ecb778eb0aa2a7866aab63db04e8ebdc1d8129
Rafał Szulik <rafblend@gmail.com>
2018-04-01 20:16:34 +0200

    Added tests for TextUtils

---
+++ DungeonGeneratorTests1/Settings/TextUtilsTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DungeonGenerator.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonGenerator.Settings.Tests
+{
+    [TestClass()]
+    public class TextUtilsTests
+    {
+        [TestMethod()]
+        public void ConvertFromHumanReadableTest()
+        {
+            string input = "Test string";
+            string output = TextUtils.ConvertFromHumanReadable(input);
+            Assert.AreEqual("TestString", output);
+        }
+
+        [TestMethod()]
+        public void ConvertToHumanReadableTest()
+        {
+            string input = "TestString";
+            string output = TextUtils.ConvertToHumanReadable(input);
+            Assert.AreEqual("Test string", output);
+        }
+    }
+}
